93e0a406d378092e09a0d8e23c21458206cd233a
richardsiqueira <richardsiqueira2004@gmail.com>
2025-08-31 15:01:39 -0400

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxKkS2aLP0xJkKsT3Wp6bPzwJQZjj_8lM",
+  authDomain: "controle-financeiro-a18de.firebaseapp.com",
+  databaseURL: "https://controle-financeiro-a18de.firebaseio.com",
+  projectId: "controle-financeiro-a18de",
+  storageBucket: "controle-financeiro-a18de.appspot.com",
+  messagingSenderId: "963247049876",
+  appId: "1:963247049876:web:be8c9355342ea3a0fbb881"
+};
+
+export default firebaseConfig;
